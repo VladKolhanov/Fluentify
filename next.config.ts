078c218e26e40ded9ddfe1a31f64fd73cb45e63a
Vlad Kolhanov <vladi.kolganov@gmail.com>
2025-03-08 19:27:20 +0200

    build(svg): add svgr
    n

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,41 @@ const nextConfig: NextConfig = {
 	locales: AVAILABLE_LOCALES,
 	defaultLocale: DEFAULT_LOCALE,
 	localeDetection: false,
+
+	webpack(config) {
+		// @ts-ignore
+		const fileLoaderRule = config.module.rules.find((rule) =>
+			rule.test?.test?.('.svg')
+		)
+
+		config.module.rules.push(
+			{
+				...fileLoaderRule,
+				test: /\.svg$/i,
+				resourceQuery: /url/,
+			},
+			{
+				test: /\.svg$/i,
+				issuer: fileLoaderRule.issuer,
+				resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+				use: ['@svgr/webpack'],
+			}
+		)
+
+		fileLoaderRule.exclude = /\.svg$/i
+
+		return config
+	},
+	experimental: {
+		turbo: {
+			rules: {
+				'*.svg': {
+					loaders: ['@svgr/webpack'],
+					as: '*.js',
+				},
+			},
+		},
+	},
 }
 
 const withNextIntl = createNextIntlPlugin()
